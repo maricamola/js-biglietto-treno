f2548b1c1d5d86252859dea53ce7fac3fb7788f5
maricamola <maricamola98@libero.it>
2023-03-20 18:06:31 +0100

    final price with discounts added

--- JS/script.js
+++ JS/script.js
@@ -1,36 +1,18 @@
-const kmDaPercorrere = prompt ('Km da percorrere')
+// costanti
+const kmDaPercorrere = prompt('Km da percorrere');
+const etaPasseggero = prompt('Età del passeggero');
+const costoChilometro = 0.21;
 
-const etaPasseggero = prompt ('Età del passeggero')
+// valori che potrebbero cambiare
+let PrezzoBiglietto = kmDaPercorrere * costoChilometro;
 
-console.log(kmDaPercorrere , etaPasseggero);
-
-const costoChilometro = 0.21
-
-const PrezzoBiglietto = kmDaPercorrere * costoChilometro;
-
-console.log(PrezzoBiglietto)
-
-let sconto = PrezzoBiglietto
-
-let sconto40 = PrezzoBiglietto - (PrezzoBiglietto * 0.4) 
+console.log(kmDaPercorrere, etaPasseggero);
+console.warn(PrezzoBiglietto);
 
 if (etaPasseggero <= 18) {
-  `
-Il costo del biglietto è ${sconto20} €
-`
+  PrezzoBiglietto = PrezzoBiglietto * 0.8;
 } else if (etaPasseggero >= 65) {
-  `
-Il costo del biglietto è ${sconto40} €
-`
-}
-else {
-  `
-Il costo del biglietto è ${PrezzoBiglietto} €
-`
+  PrezzoBiglietto = PrezzoBiglietto * 0.6;
 }
 
-
-const message =`
-Il costo del biglietto è ${PrezzoBiglietto} €
-`
-document.getElementById('output').innerHTML = message;
+document.getElementById('output').innerHTML = `Il costo del biglietto è ${PrezzoBiglietto} €`;
