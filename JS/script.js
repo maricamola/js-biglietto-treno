5f15b1ad328168eb8d82616754fd50d535f7ee2a
maricamola <maricamola98@libero.it>
2023-03-20 16:33:13 +0100

    fixed ticket cost

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,13 @@ const etaPasseggero = prompt ('Età del passeggero')
 
 console.log(kmDaPercorrere , etaPasseggero);
 
+const costoChilometro = 0.21
+
+const PrezzoBiglietto = kmDaPercorrere * costoChilometro;
+
+console.log(PrezzoBiglietto)
+
+const message =`
+Il costo del biglietto è ${PrezzoBiglietto} €
+`
+document.getElementById('output').innerHTML = message;
